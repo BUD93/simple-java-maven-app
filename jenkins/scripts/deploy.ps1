fe017284cdb1f4824e78b937bf2f38f1d5412ae2
BUD93 <burhan.arshad93@gmail.com>
2018-07-06 17:44:24 +0500

    Add files via upload

---
+++ jenkins/scripts/deploy.ps1
@@ -0,0 +1,2 @@
+﻿cd "C:\Program Files\jboss\bin"
+./jboss-cli.ps1 --connect --controller=remote+http://127.0.0.1:9990 --user=admin --password=bursys@1234 --command="undeploy my-app-1.0-SNAPSHOT.jar"
