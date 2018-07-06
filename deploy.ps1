a7498436e079b70924b0cff1ea776b8079baa7bc
BUD93 <burhan.arshad93@gmail.com>
2018-07-06 20:01:10 +0500

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,3 @@
+Copy-Item -Path "C:\Program Files (x86)\Jenkins\workspace\test\target\my-app-1.0-SNAPSHOT.jar" -Destination "C:\Program Files\jboss\bin\deployments\my-app-1.0-SNAPSHOT.jar"
 cd "C:\Program Files\jboss\bin"
-./jboss-cli.ps1 --connect --controller=remote+http://127.0.0.1:9990 --user=admin --password=bursys@1234 --command="undeploy my-app-1.0-SNAPSHOT.jar"
+./jboss-cli.ps1 --connect --controller=remote+http://127.0.0.1:9990 --user=admin --password=bursys@1234 --command="deploy /deployments/my-app-1.0-SNAPSHOT.jar"
